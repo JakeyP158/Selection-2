76fa538a569d848e48f0ec5a991da4ffecc3ca56
Jake <17f23816@marling.school>
2020-10-02 12:48:52 +0000

    ?

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from random import randrange
 wrong = "Wrong you small brian"
 print("----Welcome to the Joke Machine----")
-joke = randrange(0, 10, 2)
+joke = randrange(0, 4, 2)
 if joke == 0:
   answer = input("How do you make holy water? ")
 if answer == "boil the hell out of it":
   print("Correct, it's cringe you knew that...")
 else:
   print(wrong)
+  print("The answer was: Boil the hell out of it")
 if joke == 2:
   answer = input("What do you call monkeys who share an amazon account? ")
 if answer == "they were prime mates":
   print("Correct, that was a cringe order...")
 else:
   print(wrong)
+  print("The answer was: They were prime mates")
